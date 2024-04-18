f0e233f62a16c6b8398a50accf137d2807bbd8e6
htrzak <57521341+htrzak@users.noreply.github.com>
2024-04-18 15:53:36 +0200

    Update Unit.sh

--- Unit.sh
+++ Unit.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-UNT=$(date '+%d/%m/%Y %H:%M:%S')
-echo "Running Unit Test Cases: $UNT"
-java -jar JavaSimple.jar
+#UNT=$(date '+%d/%m/%Y %H:%M:%S')
+#echo "Running Unit Test Cases: $UNT"
+#java -jar JavaSimple.jar
+echo "Hola Unit"
